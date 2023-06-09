181502a08afc7613c1a4b15b02723db5a23830f4
Maheen_Ali_dev <125376391+MaheenAli-dev@users.noreply.github.com>
2023-06-10 00:34:03 +0500

    Add files via upload

---
+++ banopdf/assignment58and67/app.js
@@ -0,0 +1,47 @@
+
+//  1 .const mainContent = document.getElementById('main-content');
+//  console.log(mainContent);
+//  const childElements = mainContent.children;
+//  console.log(childElements);
+
+//  const renderElements = document.getElementsByClassName('render');
+//  for (let i = 0; i < renderElements.length; i++) {
+//    console.log(renderElements[i].innerHTML);
+//  }
+//  const firstNameInput = document.getElementById('first-name');
+//  firstNameInput.value = 'John';
+//  const lastNameInput = document.getElementById('last-name');
+//  const emailInput = document.getElementById('email');
+//  lastNameInput.value = 'Doe';
+//  emailInput.value = 'johndoe@example.com';
+
+
+// 2.
+// const formContent = document.getElementById('form-content');
+// console.log('Node type of element with id "form-content":', formContent.nodeType);
+
+
+// const lastNameElement = document.getElementById('lastName');
+// console.log('Node type of element with id "lastName":', lastNameElement.nodeType);
+// console.log('Node type of the child node of element with id "lastName":', lastNameElement.firstChild.nodeType);
+
+
+// lastNameElement.firstChild.nodeValue = 'Updated Last Name: Smith';
+
+// const mainContent = document.getElementById('main-content');
+// const firstChild = mainContent.firstChild;
+// const lastChild = mainContent.lastChild;
+// console.log('First child of element with id "main-content":', firstChild);
+// console.log('Last child of element with id "main-content":', lastChild);
+
+
+// const nextSibling = lastNameElement.nextSibling;
+// const previousSibling = lastNameElement.previousSibling;
+// console.log('Next sibling of element with id "lastName":', nextSibling);
+// console.log('Previous sibling of element with id "lastName":', previousSibling);
+
+
+// const emailElement = document.getElementById('email');
+// const parentNode = emailElement.parentNode;
+// console.log('Parent node of element with id "email":', parentNode);
+// console.log('Node type of element with id "email":', emailElement.nodeType);
